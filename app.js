46436d561dfe0ee7cc35a420c6641d2b2711b11a
Oleg Balitskiy <oleg.balitskiy@gmail.com>
2025-01-23 21:59:18 -0500

    put the cors above routes

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ mongoose
   })
   .catch(console.error);
 
+// enable cors
+app.use(cors());
+
 // middleware
 app.use(express.json()); // parse JSON bodies
 
 // register main routes
 app.use(routes);
 
-// enable cors
-app.use(cors());
 // start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
