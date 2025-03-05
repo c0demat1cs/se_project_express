5d3206ba1edc85474ddd38f5197708307336c810
Oleg Balitskiy <oleg.balitskiy@gmail.com>
2025-03-05 14:48:28 -0500

    issue deleting item

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
-const JWT_SECRET = process.env.JWT_SECRET || "passWord1234";
-// const { JWT_SECRET = "super-strong-secret" } = process.env;
+// const JWT_SECRET = process.env.JWT_SECRET || "passWord1234";
+const { JWT_SECRET = "dev-secret" } = process.env;
 
+console.log(JWT_SECRET);
 module.exports = {
   JWT_SECRET,
 };
